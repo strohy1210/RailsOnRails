f1a913be2c758e6a6a49dcf68358a14eecc1627c
strohy120 <sarah.stroh@flatironschool.com>
2014-11-22 16:34:45 -0500

    added unfunctional search bar

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+class SearchController < ApplicationController
+  def index
+
+    # http://streeteasy.com/for-rent/nyc/status:open%7Cprice:-2500%7Cbeds:1%7Ccommute:1800:11%20broadway,%20new%20york,%20ny%7Ctransit_distance:0.3%7Ctransit_lines:1,2,3
+  end
+end
---
+++ app/models/apartment.rb
@@ -0,0 +1,5 @@
+class Apartment < ActiveRecord::Base
+  def search
+      
+  end
+end
